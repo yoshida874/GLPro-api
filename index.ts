a0141aee8731588422669fbbb4c2775d2258b24b
yoshida874 <ud8mnzdd@gmail.com>
2021-10-22 11:17:16 +0900

    add: eslintとprettierの追加

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 const app: express.Express = express();
-import router from "./routes/apiRoute";
+import router from './routes/apiRoute';
 
-app.use("/", router);
+app.use('/', router);
 
 app.listen(8080, () => {
-  console.log("start server 8080");
+  console.log('start server 8080');
 });
