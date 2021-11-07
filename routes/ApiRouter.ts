d3a541ab2be4491ecbff20a9f093cedf4d775dd2
yoshida874 <ud8mnzdd@gmail.com>
2021-11-07 22:33:48 +0900

    remove: サンプル関数削除

--- routes/ApiRouter.ts
+++ routes/ApiRouter.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-import { about } from './about/about';
-import { area } from './area/areadetails';
+import { area } from './area/area';
 const router = Router();
 
-router.use('/about', about);
 router.use('/area', area);
 
 export const apiRouter = router;
