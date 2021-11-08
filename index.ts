a5a986bc937ff086c8f5c30c7e2d9c293f58aec4
yoshida874 <ud8mnzdd@gmail.com>
2021-11-08 14:55:39 +0900

    add: cors設定追加

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+const cors = require('cors');
 const app: express.Express = express();
 import { apiRouter } from './routes/ApiRouter';
 
 app.use('/', apiRouter);
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.listen(8080, () => {
   console.log('start server 8080');
