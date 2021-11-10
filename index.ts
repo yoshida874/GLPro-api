abf6dc3c8b1618efab28de4abf9685b238732cfb
yoshida874 <ud8mnzdd@gmail.com>
2021-11-10 15:49:30 +0900

    add: cors設定の追加

--- index.ts
+++ index.ts
@@ -3,15 +3,17 @@ const cors = require('cors');
 const app: express.Express = express();
 import { apiRouter } from './routes/ApiRouter';
 
-app.use('/', apiRouter);
+app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    optionsSuccessStatus: 200,
+    origin: '*', // アクセス許可するオリジン
+    credentials: true, // レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+    optionsSuccessStatus: 200, // レスポンスstatusを200に設定
   })
 );
+
+app.use('/', apiRouter);
 
 app.listen(8080, () => {
   console.log('start server 8080');
