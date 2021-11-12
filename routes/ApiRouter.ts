68e69322125eaee63f7cf73da2582819909dc0ef
kyosuke24 <58577250+kyosuke24@users.noreply.github.com>
2021-11-12 14:34:02 +0900

    add : レビューをポストするAPI を作成

--- routes/ApiRouter.ts
+++ routes/ApiRouter.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { area } from './area/area';
+import { review } from './review/reviewController';
+
 const router = Router();
 
 router.use('/area', area);
+router.use('/review', review);
 
 export const apiRouter = router;
