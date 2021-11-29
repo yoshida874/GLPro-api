7b862abef44abf056de341134287bb97922e410b
yoshida874 <ud8mnzdd@gmail.com>
2021-11-29 14:57:45 +0900

    clean: コメント追加

--- src/area/areaController.ts
+++ src/area/areaController.ts
@@ -4,12 +4,20 @@ import { GetArea, GetAreaDetail } from './areaInterface';
 
 @Route('area')
 export class AreaController extends Controller {
+  /**
+   * すべての地区のidと名前を返す
+   * @summary 地区一覧API
+   */
   @Get('')
   async GetArea(): Promise<GetArea[]> {
     const area = await getArea();
     return area;
   }
 
+  /**
+   * 指定された地区idの地区名、地区の詳細情報を返す。
+   * @summary 地区詳細API
+   */
   @Get('{id}')
   async GetAreaDetail(@Path() id: string): Promise<GetAreaDetail> {
     const res = await getAreaDetail(id);
--- src/review/reviewController.ts
+++ src/review/reviewController.ts
@@ -4,6 +4,10 @@ import { PostReviewBody } from './reviewInterface';
 
 @Route('review')
 export class ReviewController extends Controller {
+  /**
+   * レビューを登録する
+   * @summary レビュー登録API
+   */
   @Post('create')
   async PostReview(@Body() body: PostReviewBody): Promise<void> {
     try {
